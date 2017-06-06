db6c0cf800bb608aab3310c5cec946ec5971d78e
pirosikick <pirosikick@gmail.com>
2017-06-06 15:29:03 +0900

    Rename & remove

--- examples/performance-fields/entry.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './hoge';
-
-import('./component').then(Component => {
-  ReactDOM.render(<Component />, document.getElementById('app'));
-});
